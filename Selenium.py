436b304d532888dbed351651c0643389520b829c
Goksu KARA <goksukaran@gmail.com>
2018-12-24 06:03:15 +0100

    Dropdown is not wokring

--- Selenium.py
+++ Selenium.py
@@ -11,31 +11,48 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.keys import Keys
 from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.support.ui import Select
 
 
 class selenium():
 
-    def __init__(self, cityname, districts_list):
+    def __init__(self, cityname, username, password):
         self.cityname = cityname
-        self.disrits = districts_list
+        self.username = username
+        self.password = password
+        self.browser = 0
         
     def login(self):
-        dir_path = os.path.dirname(os.path.realpath(__file__))
-        print(dir_path)
-        browser = webdriver.Chrome(ChromeDriverManager().install())
-        browser.get('http://www.google.com')
+        self.browser = webdriver.Chrome(ChromeDriverManager().install())
+        self.browser.get('https://www.wg-gesucht.de/')
         
-        search = browser.find_element_by_name('q')
-        search.send_keys("google search through python")
-        search.send_keys(Keys.RETURN)  # hit return after you enter search text
-        time.sleep(5)  # sleep for 5 seconds so you can see the results
-        browser.quit()
+        text_box_username = self.browser.find_element_by_xpath('//*[@id="login_email_username"]')
+        text_box_pass = self.browser.find_element_by_xpath('//*[@id="login_password"]')
+        submit_button = self.browser.find_element_by_xpath(' //*[@id="login_submit"]')
+        login_button = self.browser.find_element_by_xpath('//*[@id="headbar_wrapper"]/div[2]/a[2]').click()
+        time.sleep(2)
+        
+        text_box_username.send_keys(self.username)
+        text_box_pass.send_keys(self.password)
+        submit_button.click()
+        time.sleep(2)
     
     def islogin_succsessful(self):
         pass
 
-    def send_city_quarry(self):
-        pass
+    def send_city_query(self):
+       text_box = self.browser.find_element_by_xpath('//*[@id="autocompinp"]')
+       drop_menu = Select(self.browser.find_element_by_xpath('//*[@id="rubrik-dropdown"]'))
+       
+       text_box.send_keys(self.cityname)
+       drop_menu.select_by_visible_text('WG-Zimmer')
+       
+       time.sleep(2)
+       submit_button = self.browser.find_element_by_xpath('//*[@id="search_button"]').click()
 
     def send_message(self):
         pass
+
+    def quit(self):
+        self.browser.quit()
+        
--- main.py
+++ main.py
@@ -7,14 +7,25 @@ global cityname, districts_list
 
 cityname = "Hamburg"
 districts_list = []
+username="goksukaran@gmail.com"
+password="hfXFpRhNZiJza9J"
 
 from Selenium import selenium
+from web_scraping import return_wg_list
 
-
+global temp
+temp="https://www.wg-gesucht.de/wohnungen-in-Hamburg.55.2.1.0.html"
 def main():
     print("python main function")
-    selenium_instance = selenium(cityname, districts_list)
+    selenium_instance = selenium(cityname,username,password)
     selenium_instance.login()
+    selenium_instance.send_city_query()
+    print(selenium_instance.browser.current_url)
+    wg_list=return_wg_list(temp)
+    
+    print(len(wg_list))
+    for i in wg_list:
+        print(i)
 
 if __name__ == '__main__':
     main()
