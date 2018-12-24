ed7246a38f67507cd6da104b9e08b1fa787ff2d8
Goksu KARA <goksukaran@gmail.com>
2018-12-24 01:43:37 +0100

    Simple coonectuon ok

--- Selenium.py
+++ Selenium.py
@@ -3,20 +3,39 @@ Created on 24 Dec 2018
 
 @author: goksukara
 '''
+import time
+import os 
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.keys import Keys
+from webdriver_manager.chrome import ChromeDriverManager
 
 
 class selenium():
 
-    def __init__(self,cityname,districts_list):
+    def __init__(self, cityname, districts_list):
         self.cityname = cityname
         self.disrits = districts_list
         
-    def _login(self):
-        pass
+    def login(self):
+        dir_path = os.path.dirname(os.path.realpath(__file__))
+        print(dir_path)
+        browser = webdriver.Chrome(ChromeDriverManager().install())
+        browser.get('http://www.google.com')
+        
+        search = browser.find_element_by_name('q')
+        search.send_keys("google search through python")
+        search.send_keys(Keys.RETURN)  # hit return after you enter search text
+        time.sleep(5)  # sleep for 5 seconds so you can see the results
+        browser.quit()
     
     def islogin_succsessful(self):
         pass
+
     def send_city_quarry(self):
         pass
+
     def send_message(self):
         pass
--- main.py
+++ main.py
@@ -8,13 +8,13 @@ global cityname, districts_list
 cityname = "Hamburg"
 districts_list = []
 
-import selenium
+from Selenium import selenium
 
 
 def main():
     print("python main function")
     selenium_instance = selenium(cityname, districts_list)
-
+    selenium_instance.login()
 
 if __name__ == '__main__':
     main()
